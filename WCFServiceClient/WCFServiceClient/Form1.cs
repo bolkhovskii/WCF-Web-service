40a718826f7fcb763bb98b83247263102d3694c8
Bolkhovskij <bolkhovskiy.a@gmail.com>
2016-06-28 15:36:42 +0300

    WCF Service & Client

---
+++ WCFServiceClient/WCFServiceClient/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WCFServiceClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            var Remote = new localhost.Service1();
+            var Answer = Remote.ToDo(name);
+            MessageBox.Show(Answer);
+        }
+    }
+}
